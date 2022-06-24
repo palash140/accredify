4a5fd7cdc01b1d589eab8ebc745d7c7ac65dafbe
palash <palash.chandel@gmail.com>
2022-06-24 13:57:23 +0530

    Palash|Intial Commit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+
+//use PHPUnit\Framework\TestCase;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
